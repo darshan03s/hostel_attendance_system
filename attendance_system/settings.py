c7f1e76d9f4bbb69e9ec993f513b9ba79f41898d
Darshan S <darshans2727@gmail.com>
2024-03-04 23:12:21 +0530

    added default as mysql

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -75,20 +75,32 @@ WSGI_APPLICATION = 'attendance_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+#     'mysql': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'final_dbms',
+#         'USER': 'darshans',
+#         'PASSWORD': 'darshans',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'mysql': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'final_dbms',
         'USER': 'darshans',
         'PASSWORD': 'darshans',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
+
 
 
 
