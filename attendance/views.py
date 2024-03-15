d12bff814b2962ee32c803ecd8c41a95ded6a50c
Darshan S <darshans2727@gmail.com>
2024-03-15 12:35:33 +0530

    minor changes

--- attendance/views.py
+++ attendance/views.py
@@ -1,66 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from datetime import datetime, date, time
-import datetime as dt
-from attendance.models import Register, LoginLogout
-import re
-from attendance.update_attendance import update_attendance
+from .custom.operations import * 
+from django.contrib import messages
 
-""" Set this flag to false , when attendance is updated at 21:00 , this flag becomes true. """
 update_executed_today = False
-
-""" Function to check if usn is valid"""
-def validate_usn(usn):
-    usn_pattern = re.compile(r'^4GM\d\d[A-Z][A-Z]\d\d\d$')
-    return usn_pattern.match(usn) is not None
-
-"""Function to create record of student registration
-Details stored - Name, USN, Branch, Phone , Date of Registration. Password"""
-def register_student(request):
-    """Get data from the html form"""
-    
-    regusername = request.POST.get('regusername')
-    regusn = request.POST.get('regusn')
-    if not validate_usn(regusn):
-        return HttpResponse('Invalid USN')                              #returns a blank page response with only text
-    regbranch = request.POST.get('regbranch')
-    regphone = request.POST.get('regphone')
-    regpassword = request.POST.get('regpassword')
-    regdate = dt.date.today()                                            #get current date and time
-    
-    """Create register object and save the details"""
-    registration = Register(STUDENT_NAME=regusername, USN=regusn, BRANCH=regbranch, PHONE=regphone, PASSWORD=regpassword, REGISTRATION_DATE=regdate)
-    registration.save()
-
-""" Function to create records of login and logout activity of students
-Details stored - USN, Login date-time"""
-def login_logout_student(request, action):
-    """Get data from the html form"""
-    
-    logusn = request.POST.get('logusn')
-    if not validate_usn(logusn):                                        #validate usn
-        return HttpResponse('Invalid USN format.')
-    try:
-        register_instance = Register.objects.get(USN=logusn)            #check if student has registered
-    except Register.DoesNotExist:
-        return HttpResponse('You are not registered.')
-    logpassword = request.POST.get('logpassword')
-    passwords = Register.objects.values_list('USN', 'PASSWORD')
-    passwords_list = list(passwords)
-    usn_pass_dict = {usn: password for usn, password in passwords_list}
-    if logpassword != usn_pass_dict[logusn]:                            #check if usn matches with students created password
-        return HttpResponse('Invalid Password')
-    status = action.upper()                                             #convert string to uppercase
-    curr_time = datetime.today().strftime("%Y-%m-%d %H:%M:%S")          #store login date-time
-    login_logout = LoginLogout(USN=register_instance, PASSWORD=logpassword, STATUS=status, LOGIN_LOGOUT_TIME=curr_time)
-    login_logout.save()
-    
-def test():
-    update_attendance()
-    print('Attendance Updated')
 
 def index(request):
     global update_executed_today
-    if request.method == 'POST':                                        #request sent from html form to backend using POST http-request
+    if request.method == 'POST':        
         if 'action' in request.POST:
             action = request.POST['action']
             if action == 'register':
@@ -69,17 +16,16 @@ def index(request):
                 login_logout_student(request, action)
             elif action == 'logout':
                 login_logout_student(request, action)
-
-    current_time = datetime.now().time()                                #to only check attendance for todays date
-    cutoff_datetime = datetime.combine(date.today(), time(21, 0))       #set cut-off time to 21:00 
-
+                
+    current_time = datetime.now().time()                                
+    cutoff_datetime = datetime.combine(date.today(), time(21, 0))      
     if current_time > cutoff_datetime.time() and not update_executed_today:     
-        update_attendance()                                             #update_attendance() runs once a day at 21:00 for todays date
-        print('Attendance Updated')
-        update_executed_today = True                                    #update flag to true after function executed once once
+        update_attendance()                         
+        messages.success(request, f'Attendance updated at {datetime.now().strftime("%H:%M:%S")}')
+        update_executed_today = True
         
-        
-    """For testing purpose"""
-    # test()
-
-    return render(request, 'index.html')                                #return the html page stored in templates folder using render method
+    """For testing"""
+    update_attendance()                         
+    messages.success(request, f'Attendance updated at {datetime.now().strftime("%H:%M:%S")}')
+    
+    return render(request, 'index.html')        
--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -75,21 +75,14 @@ WSGI_APPLICATION = 'attendance_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'darshans',
         'PASSWORD': 'darshans',
-        'HOST': 'localhost',  # or the hostname where your MySQL server is running
-        'PORT': '3306',      # or the port on which your MySQL server is listening
+        'HOST': 'localhost',  
+        'PORT': '3306',      
     }
 }
 
